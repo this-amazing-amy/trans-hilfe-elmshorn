fd4ff795afbd55afd139fb5009c221c6270d61bf
this-amazing-amy <ameliaheydorn@gmail.com>
2024-09-17 13:06:38 +0200

    Add message for skipping todays meeting

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,54 +1,55 @@
 import {
-  isSameDay,
-  isAfter,
-  isToday,
-  endOfMonth,
-  startOfMonth,
-  isTuesday,
   addMonths,
+  differenceInDays,
   eachDayOfInterval,
-  parseISO,
+  endOfMonth,
   intlFormatDistance,
-  differenceInDays
-} from 'date-fns'
-import { range } from 'ramda'
+  isAfter,
+  isSameDay,
+  isToday,
+  isTuesday,
+  parseISO,
+  startOfMonth,
+} from "date-fns";
+import { range } from "ramda";
 
-const ignoredDates = [
-  parseISO('2022-12-20')
-]
+const ignoredDates = [parseISO("2022-12-20"), parseISO("2024-09-17")];
 
 const thirdTuesdayOfMonth = (dateInMonth: Date) =>
   eachDayOfInterval({
     start: startOfMonth(dateInMonth),
-    end: endOfMonth(dateInMonth)
+    end: endOfMonth(dateInMonth),
   })
-    .filter(isTuesday).at(2)!
+    .filter(isTuesday)
+    .at(2)!;
 
-const isNotIgnoredDate = (date: Date) => !ignoredDates.some(d => isSameDay(d, date))
+const isNotIgnoredDate = (date: Date) =>
+  !ignoredDates.some((d) => isSameDay(d, date));
 
 const format = (date: Date) => {
-  const distance = differenceInDays(date, new Date())
-  const abs = Intl.DateTimeFormat('de-DE', {
-    weekday: 'long',
-    day: '2-digit',
-    month: 'long',
-    year: 'numeric'
-  }).format(date)
+  const distance = differenceInDays(date, new Date());
+  const abs = Intl.DateTimeFormat("de-DE", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(date);
   const rel = intlFormatDistance(date, new Date(), {
-      locale: 'de',
-      unit: 'day'
-    })
+    locale: "de",
+    unit: "day",
+  });
 
   if (distance < 3) {
-    return `${rel}, am ${abs}`
+    return `${rel}, am ${abs}`;
   }
-  return `am ${abs}`
-}
+  return `am ${abs}`;
+};
 
-export const nextGroupDates = () => range(0, 100)
-  .map(x => addMonths(new Date(), x))
-  .map(thirdTuesdayOfMonth)
-  .filter(isNotIgnoredDate)
-  .filter(d => isToday(d) || !isAfter(new Date(), d!))
+export const nextGroupDates = () =>
+  range(0, 100)
+    .map((x) => addMonths(new Date(), x))
+    .map(thirdTuesdayOfMonth)
+    .filter(isNotIgnoredDate)
+    .filter((d) => isToday(d) || !isAfter(new Date(), d!));
 
-export const nextGroupDate = () => format(nextGroupDates().at(0)!)
+export const nextGroupDate = () => format(nextGroupDates().at(0)!);
